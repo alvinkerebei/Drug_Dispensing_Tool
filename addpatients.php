8f9a73cd2801d83dc93d6f5529de85d6bd25866d
Kerebei <alvin.kerebei@strathmore.edu>
2023-06-23 14:22:25 +0300

    Some changes to the whole patients table and how the interface looks

--- addpatients.php
+++ addpatients.php
@@ -2,52 +2,132 @@
 
 require_once 'dbconnection.php';
 
-if(isset($_POST['submit'])){
+$Patient_Name="";
+$Patient_Address="";
+$Patient_Age="";
+$Doctor_SSN="";
 
-$Patient_SSN= $_POST['id'];
-$Patient_Name = $_POST['name'];
-$Patient_Address = $_POST['address'];
-$Patient_Age = $_POST['age'];
-$Doctor_SSN = $_POST['doc_id'];
-
-// Prepare the SQL statement
-$query = "INSERT INTO `patients`(`Patient_SSN`, `Patient_Name`, `Patient_Address`, `Patient_Age`, `Doctor_SSN`) 
-        VALUES ('$Patient_SSN','$Patient_Name','$Patient_Address','$Patient_Age','$Doctor_SSN')";
+$errorMessage="";
+$successMessage="";
 
 
-// Execute the query
-$run = mysqli_query($conn,$query);
 
-if($run){
-    echo' Added';
-}else{
-    echo"Error" .$sql;
+if($_SERVER['REQUEST_METHOD']=='POST'){
+
+    $Patient_Name = $_POST["name"];
+    $Patient_Address = $_POST["address"];
+    $Patient_Age = $_POST["age"];
+    $Doctor_SSN = $_POST["doc_id"];
+
+    do{
+        if(empty($Patient_Name)||empty($Patient_Address)||empty($Patient_Age)||empty($Doctor_SSN)){
+            $errorMessage = "A Field is Empty!";
+            break;
+        }
+        //add patient to db
+        $sql ="INSERT INTO `patients`(`Patient_Name`, `Patient_Address`, `Patient_Age`, `Doctor_SSN`) 
+        VALUES ('$Patient_Name','$Patient_Address','$Patient_Age','$Doctor_SSN')";
+        $result= $conn->query($sql);
+
+        if (!$result){
+            $errorMessage = "Invalid". $conn->error;
+            break;
+        }
+        $Patient_Name="";
+        $Patient_Address="";
+        $Patient_Age="";
+        $Doctor_SSN="";
+
+        $successMessage ="Patient is Successfully Added";
+
+        header("location:/Drug_Dispensing_Tool/patientpage.php");
+        exit;
+        
+    }while(false);
+
+
 }
-}
-// Close the connection
-$conn->close();
+
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Create New User</title>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content = "IE=edge">
+    <meta name = "viewport" contnt ="width=device-width, initial-scale=1.0">
+    <title>Add New Patient</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <script src="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-    <h1>Create New User</h1>
-    <form action="addpatients.php" method="POST">
-        
-        <input type="number" name="id" required placeholder="ID NO."><br><br>      
-      
-        <input type="text" name="name"  required placeholder="Name"><br><br>
-      
-        <input type="text" name="address" required placeholder="Address"><br><br>
-       
-        <input type="number" name="age"  required placeholder="Age:"><br><br>
-       
-        <input type="number" name="doc_id" required placeholder="Doctor ID"><br><br>
-        
-        <input type="submit" name='submit' value="Create">
-    </form>
+    <div class="container my-5">
+        <h1>Create New User</h1>
+
+        <?php
+        if(!empty($errorMessage)){
+            echo"
+            <div class='alert alert-warning alert-dismissable fade show' role='alert'>
+                <strong>$errorMessage</strong>
+                <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+            </div>
+            ";
+        }
+        ?> 
+        <form action="addpatients.php" method="POST">     
+
+        <div class="row mb-3">
+            <label class ="col-sm-3 col-form-label">Name</label>
+            <div class="col-sm-6">
+                <input type="text" class="form-control" name="name" value="<?php echo $Patient_Name ?>">
+            </div>
+        </div>
+
+        <div class="row mb-3">
+            <label class ="col-sm-3 col-form-label">Address</label>
+            <div class="col-sm-6">
+                <input type="text" class="form-control" name="address" value="<?php echo $Patient_Address ?>">
+            </div>
+        </div>
+
+        <div class="row mb-3">
+            <label class ="col-sm-3 col-form-label">Age</label>
+            <div class="col-sm-6">
+                <input type="number" class="form-control" name="age" value="<?php echo $Patient_Age ?>">
+            </div>
+        </div>
+
+        <div class="row mb-3">
+            <label class ="col-sm-3 col-form-label">Doctor ID</label>
+            <div class="col-sm-6">
+                <input type="number" class="form-control" name="doc_id" value="<?php echo $Doctor_SSN ?>">
+            </div>
+        </div>
+
+        <?php
+        if(!empty($successMessage)){
+            echo"
+            <div class='row mb-3'>
+                <div class='offset-sm-3 col-sm-6'>
+                    <div class='alert alert-warning alert-dismissable fade show' role='alert'>
+                    <strong>$successMessage</strong>
+                    <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                    </div>
+                </div>
+            </div>
+            ";
+        }
+        ?>
+        <div class="row mb-3">
+            <div class="offset-sm-3 col-sm-3 d-grid">
+                <button type="submit" class="btn btn-primary" >Submit</button>
+            </div>
+            <div class="col-sm-3 d-grid">
+                <a class="btn btn-outline-primary" href="/Drud_Dispensing_Tool/patientpage.php" role="button">Cancel</a>
+            </div>
+        </div>
+        </form>
+    </div>
 </body>
 </html>
--- patientpage.php
+++ patientpage.php
@@ -1,91 +1,61 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content = "IE=edge">
+    <meta name = "viewport" contnt ="width=device-width, initial-scale=1.0">
     <title>Heisenberg Drug Dispensing Tool</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 20px;
-        }
-        h1 {
-            color: #333;
-            text-align: center;
-        }
-        .container {
-            max-width: 600px;
-            margin: 0 auto;
-        }
-        .form-group {
-            margin-bottom: 20px;
-        }
-        .form-group label {
-            display: block;
-            font-weight: bold;
-            margin-bottom: 5px;
-        }
-        .form-group input {
-            width: 100%;
-            padding: 10px;
-            font-size: 16px;
-        }
-        .form-group input[type="submit"] {
-            background-color: #4CAF50;
-            color: white;
-            border: none;
-            cursor: pointer;
-        }
-    </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
     <div class="container">
         <h1>Welcome to Heisenberg Drug Dispensing Tool</h1>
         <h2>Get well Soon :)</h2>
-        
+        <a class = "btn btn-primary" href = "/Drug_Dispensing_Tool/addpatients.php" role="button">Add Patient</a> <br>
+           <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Address</th>
+                    <th>Age</th>
+                    <th>Doctor ID</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+            require_once "dbconnection.php";
+            // Retrieve the data from the database
+            $sql = "SELECT * FROM patients";
+            $result = $conn->query($sql);
 
+            if(!$result){
+                die("Error: ".$conn->error);
+            }
 
-                
-            </div>
+            while($row = $result->fetch_assoc()){
+                echo"
+                <tr>
+                <td>$row[Patient_SSN]</td>
+                <td>$row[Patient_Name]</td>
+                <td>$row[Patient_Address]</td>
+                <td>$row[Patient_Age]</td>
+                <td>$row[Doctor_SSN]</td>
+                <td>
+                <a class = 'btn btn-primary btn-sm' href = '/Drug_Dispensing_Tool/addpatients.php?id=$row[Patient_SSN]'>Edit</a> 
+                <a class = 'btn btn-primary btn-sm' href = '/Drug_Dispensing_Tool/addpatients.php?id=$row[Patient_SSN]'>Delete</a> 
+                </td>
+            </tr>
+                ";
+            }
+            ?>
+               
+            </tbody>
+           </table>
+    </div>
         </form>
     </div>
 </body>
 </html>
 
-<?php
-require_once "dbconnection.php";
-
-// Retrieve the data from the database
-$sql = "SELECT * FROM patients";
-$result = $conn->query($sql);
-
-// Check if any rows are returned
-if ($result->num_rows > 0) {
-    // Generate the HTML table headers
-    echo "<table>";
-    echo "<tr>";
-    echo "<th>Patient_SSN</th>";
-    echo "<th>Patient_Name</th>";
-    echo "<th>Patient_Address</th>";
-    echo "<th>Patient_Age</th>";
-    echo "<th>Doctor_SSN</th>";
-    echo "</tr>";
-
-    // Output data of each row
-    while ($row = $result->fetch_assoc()) {
-        echo "<tr>";
-        echo "<td>" . $row["Patient_SSN"] . "</td>";
-        echo "<td>" . $row["Patient_Name"] . "</td>";
-        echo "<td>" . $row["Patient_Address"] . "</td>";
-        echo "<td>" . $row["Patient_Age"] . "</td>";
-        echo "<td>" . $row["Doctor_SSN"] . "</td>";
-        echo "</tr>";
-    }
-
-    echo "</table>";
-} else {
-    echo "No records found";
-}
-
-// Close the database connection
-$conn->close();
-?>
